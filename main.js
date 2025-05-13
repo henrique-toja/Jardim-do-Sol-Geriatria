1812eed121ed16ee83365b0b5dd520beae80fee3
Henrique Toja <168424648+henrique-toja@users.noreply.github.com>
2025-05-13 12:46:47 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -91,3 +91,5 @@ servicesPrev.addEventListener('click', () => {
 
 // Inicialização
 updateServiceCarousel();
+
+(function(a,b,c,d){try{var e=b.head||b.getElementsByTagName("head")[0];var f=b.createElement("script");f.setAttribute("src",c);f.setAttribute("charset","UTF-8");f.defer=true;a.neuroleadId=d;e.appendChild(f)}catch(g){}})(window,document,"https://cdn.leadster.com.br/neurolead/neurolead.min.js","n1KtVCoHGEyvTCCNhhJf9P6p3")
