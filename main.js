1a0d6100564651623d51a33707e17799e3d183dd
Henrique Toja <168424648+henrique-toja@users.noreply.github.com>
2025-05-13 12:46:25 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// Facebook Pixel (Meta Pixel) -----------------------------
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1729771167889310');
+fbq('track', 'PageView');
+
 // Carrossel de Acomodações
 const acomodacoesSlides = document.querySelectorAll('.acomodacoes-slide');
 const acomodacoesPrev = document.querySelector('.acomodacoes-carousel-btn.prev');
